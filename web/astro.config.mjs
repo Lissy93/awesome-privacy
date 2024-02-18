d479d48d2c1c732b8eb525f6e502d582d1ca8d78
Alicia Sykes <alicia@omg.lol>
2024-02-18 19:30:20 +0000

    Adds Svelte integration, for dynamic components

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [svelte()]
+});
