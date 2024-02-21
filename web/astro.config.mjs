a1cb66ceac6c64784246244c108ebb402c74df7d
Alicia Sykes <alicia@omg.lol>
2024-02-21 22:59:01 +0000

    Adds Partytown and sitemap integrations

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 
-import svelte from "@astrojs/svelte";
+import svelte from '@astrojs/svelte';
+import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [
+    svelte(),
+    partytown(),
+    sitemap(),
+  ],
+  site: import.meta.env.SITE_URL || 'https://awesome-privacy.xyz',
 });
