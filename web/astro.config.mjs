2c9ebdb919d7f4cd1a7d7d8cbe91ea0290a61578
Alicia Sykes <alicia@omg.lol>
2024-02-24 21:40:51 +0000

    Use Vercel adapter

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import svelte from '@astrojs/svelte';
 import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
+import vercel from "@astrojs/vercel/serverless";
+// import netlify from "@astrojs/netlify";
 
 export default defineConfig({
-  integrations: [
-    svelte(),
-    partytown(),
-    sitemap(),
-  ],
+  output: 'hybrid',
+  integrations: [svelte(), partytown(), sitemap()],
   site: import.meta.env.SITE_URL || 'https://awesome-privacy.xyz',
+  adapter: vercel(),
+  // adapter: netlify(),
 });
