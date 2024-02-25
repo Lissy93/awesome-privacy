6a6ba907fcb7be36d1d1397ee1a30a430c0c3bce
Alicia Sykes <alicia@omg.lol>
2024-02-25 15:27:58 +0000

    Ready for deploy

--- web/src/utils/fetch-data.ts
+++ web/src/utils/fetch-data.ts
@@ -3,13 +3,15 @@ import yaml from 'js-yaml';
 
 import type { AwesomePrivacy } from '../types/Service';
 
+const awesomePrivacyData = 'https://raw.githubusercontent.com/Lissy93/awesome-privacy/main/awesome-privacy.yml';
+
 export const fetchData = async (): Promise<AwesomePrivacy> => {
-  return await fetch('http://localhost:4321/awesome-privacy.yml')
+  return await fetch(awesomePrivacyData)
     .then((res) => res.text())
     .then((data) => yaml.load(data))
     .catch((err) => console.error('ah crap', err)) as AwesomePrivacy;
 }
 
 export const slugify = (title: string) => {
-  return title.toLowerCase().replace(/\s/g, '-');
+  return title.toLowerCase().replace(/\s/g, '-');z
 };
