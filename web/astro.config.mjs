3a9917a6317a90a4e4e0855e38f938646827afe6
Alicia Sykes <alicia@omg.lol>
2024-03-01 18:05:15 +0000

    Select the apprpriate Astro adapter based on hosting provider

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,14 +1,37 @@
 import { defineConfig } from 'astro/config';
+
+// Integrations
 import svelte from '@astrojs/svelte';
 import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
-import vercel from "@astrojs/vercel/serverless";
-// import netlify from "@astrojs/netlify";
 
-export default defineConfig({
-  output: 'hybrid',
-  integrations: [svelte(), partytown(), sitemap()],
-  site: import.meta.env.SITE_URL || 'https://awesome-privacy.xyz',
-  adapter: vercel(),
-  // adapter: netlify(),
-});
+// Adapters
+import vercelAdapter from '@astrojs/vercel/serverless';
+import netlifyAdapter from '@astrojs/netlify';
+import nodeAdapter from '@astrojs/node';
+import cloudflareAdapter from '@astrojs/cloudflare';
+
+// Determine the deploy target (vercel, netlify, cloudflare, node)
+const deployTarget = import.meta.env.DEPLOY_TARGET || 'vercel';
+
+// Determine the output mode (server or hybrid)
+const output = import.meta.env.OUTPUT || 'hybrid';
+
+// The FQDN of where the site is hosted (used for sitemaps & canonical URLs)
+const site = import.meta.env.SITE_URL || 'https://awesome-privacy.xyz';
+
+// Initialize Astro integrations
+const integrations = [svelte(), partytown(), sitemap()];
+
+// Set the appropriate adapter, based on the deploy target
+const adapter = {
+  vercel: vercelAdapter(),
+  netlify: netlifyAdapter(),
+  cloudflare: cloudflareAdapter(),
+  node: nodeAdapter({
+    mode: 'standalone',
+  }),
+}[deployTarget];
+
+// Export Astro configuration
+export default defineConfig({ output, integrations, site, adapter });
