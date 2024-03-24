f6aeaf057384e663310a6688a760a4049957fd11
Alicia Sykes <alicia@omg.lol>
2024-03-24 13:00:18 +0000

    Fetch info about discord server

---
+++ web/src/utils/fetch-discord-info.ts
@@ -0,0 +1,21 @@
+
+export const fetchDiscordInfo = async (discordInvite: string): Promise<DiscordInfo | null> => {
+  const endpoint = `https://discord-invite-info.as93.net/${discordInvite}`;
+  try {
+    return await fetch(endpoint).then((res) => res.json());
+  } catch (error) {
+    console.error('Error fetching discord data:', error);
+    return null;
+  }
+};
+
+export interface DiscordInfo {
+  inviteCode: string;
+  name: string;
+  memberCount: number;
+  memberOnlineCount: number;
+  channel: string;
+  icon: string;
+  banner: string;
+  inviter: string | null;
+}
